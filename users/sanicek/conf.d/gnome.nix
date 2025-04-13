806d0c0fbbbabd1b9b870a490b9fdde8f4f582dc
Marian Vencel <marian.vencel@gmail.com>
2025-04-14 01:40:13 +0200

    cfg: gnome wallpaper

--- users/sanicek/conf.d/gnome.nix
+++ users/sanicek/conf.d/gnome.nix
@@ -3,6 +3,15 @@
 {
   dconf = {
     enable = true;
-    settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+    settings= {
+      "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+      "org/gnome/desktop/background" = {
+        color-shading-type = "solid";
+        picture-uri = "file://${config.home.homeDirectory}/Pictures/wallpaper.jpg";
+        picture-uri-dark = "file://${config.home.homeDirectory}/Pictures/wallpaper.jpg";
+        picture-options = "fit";
+      };
+    };
+
   };
 }
