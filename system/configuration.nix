a9ea543b2973c7662e789e0db7b3e53686013670
Marian Vencel <marian.vencel@gmail.com>
2025-04-15 18:13:03 +0200

    cfg(system): open port for input leap

--- system/configuration.nix
+++ system/configuration.nix
@@ -122,7 +122,7 @@
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 24800 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
